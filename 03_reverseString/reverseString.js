e7568ab06baa16498c6bb6ba72f974ab7b614f20
Dylante <dwmo616@gmail.com>
2024-03-19 12:53:17 -0400

    edit to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    temp = '';
+    for (i = string.length-1; i >= 0; i--) {
+        temp = temp + string.charAt(i);
+    }
 
+    return temp;
 };
 
 // Do not edit below this line
