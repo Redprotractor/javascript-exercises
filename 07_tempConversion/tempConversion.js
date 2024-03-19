23da417a2c58183b5dc5ffd527762b79f69d14e2
Dylante <dwmo616@gmail.com>
2024-03-19 19:44:38 -0400

    edits to convert functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  temp = (f - 32) * 5 / 9;
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  deg = (c * 9 / 5) + 32;
+  return Math.round(deg * 10) / 10;
 };
 
 // Do not edit below this line
